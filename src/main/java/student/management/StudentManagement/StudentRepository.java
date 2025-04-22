48474e3567784fd9718f11ccb0c8a25eacd4902b
taiki <tik86_trueno@yahoo.co.jp>
2025-04-22 15:27:13 +0900

    lombokを使ってread処理

--- src/main/java/student/management/StudentManagement/StudentRepository.java
+++ src/main/java/student/management/StudentManagement/StudentRepository.java
@@ -1,27 +1,13 @@
 package student.management.StudentManagement;
 
 import java.util.List;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface StudentRepository {
 
-  @Select("SELECT * FROM student")
-  List<Student> getAllStudent();
+  @Select("SELECT * FROM students")
+  List<Student> search();
 
-  @Select("SELECT * FROM student WHERE name = #{name}")
-  Student searchByName(String name);
-
-  @Insert("INSERT student values(#{name}, #{age})")
-  void registerStudent(String name, int age);
-
-  @Update("UPDATE student set age = #{age} WHERE name = #{name}")
-  void updateStudent(String name, int age);
-
-  @Delete("DELETE FROM student WHERE name = #{name}")
-  void deleteStudent(String name);
 }
