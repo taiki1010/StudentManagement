0f5dbb632ef4ceda48db1c07f67c31d3b8c7eb23
taiki <tik86_trueno@yahoo.co.jp>
2025-05-02 16:01:54 +0900

    OpenAPIでドキュメントを作成

--- src/main/java/student/management/StudentManagement/StudentManagementApplication.java
+++ src/main/java/student/management/StudentManagement/StudentManagementApplication.java
@@ -1,8 +1,11 @@
 package student.management.StudentManagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "受講生管理システム", description = "受講生を管理するためのシステムです。"))
 @SpringBootApplication
 public class StudentManagementApplication {
 
