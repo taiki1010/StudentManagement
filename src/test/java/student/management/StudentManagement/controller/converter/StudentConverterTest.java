fd6a3ac9d2265e1bbf01cdb5d7212327a86fdc0f
taiki <tik86_trueno@yahoo.co.jp>
2025-05-09 07:57:49 +0900

    converterのテスト実装_修正

--- src/test/java/student/management/StudentManagement/controller/converter/StudentConverterTest.java
+++ src/test/java/student/management/StudentManagement/controller/converter/StudentConverterTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import student.management.StudentManagement.data.Student;
 import student.management.StudentManagement.data.StudentCourse;
@@ -12,15 +11,9 @@ import student.management.StudentManagement.domain.StudentDetail;
 
 class StudentConverterTest {
 
-  private StudentConverter sut;
-
-  @BeforeEach
-  void before() {
-    sut = new StudentConverter();
-  }
-
   @Test
   void 受講生リストと受講生コースリストを引数にして実行されると受講生詳細リストが返却されること() {
+    StudentConverter sut = new StudentConverter();
     Student student = new Student("1", "田中太郎", "タナカタロウ", "タロタロ", "tarou@example.com",
         "東京", 20, "男性", "", false);
     LocalDateTime now = LocalDateTime.now();
